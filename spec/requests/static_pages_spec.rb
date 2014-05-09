24dd47e419d484128983c3b376e0de9325c3cfcc
phungnc <phungnc@gmail.com>
2014-05-09 23:17:21 +0700

    finish filling in layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,16 @@ describe "StaticPages" do
 
   subject {page}
 
+  shared_examples_for "all static pages" do 
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it {should have_content('Picat CMS')}
+    let(:heading) { 'Picat CMS' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
   end
 
 
@@ -17,10 +24,19 @@ describe "StaticPages" do
   end
 
   describe "Contact page" do
-    before { visit contact_path }
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    before { visit contact_path } 
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
   end
 
+  # test links on layout
+  it "should have the right links on the layout" do
+
+    visit root_path
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+
+  end
 
 end
