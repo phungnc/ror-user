d808c69393d6fc19ec52cb277d33720c6bad8269
phungnc <phungnc@gmail.com>
2014-05-10 07:17:26 +0700

    make comment better

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,10 @@
 include ApplicationHelper
 
 #def full_title(page_title)
- # base_title = "Picat CMS"
-  #if page_title.empty?
-   # base_title
-  #else
-   # "#{base_title} | #{page_title}"
-  #end
+# base_title = "Picat CMS"  
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+# end
 #end
