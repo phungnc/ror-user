40a2169944b9c7275dbb2473f2b00d78f0066f84
phungnc <phungnc@gmail.com>
2014-05-11 08:44:50 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
-  #this is comment in feature/test_other_feature branch
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # handle a successfule save.
+      flash[:success] = "Welcome to the PitCat CMS"
+      redirect_to @user
+    else 
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
