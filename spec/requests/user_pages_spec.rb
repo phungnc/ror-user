4b432c849e2e6b20215b7d96a15991d000ec98a3
phungnc <phungnc@gmail.com>
2014-05-12 21:16:07 +0700

    init test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,8 +62,8 @@ describe "UserPages" do
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
-      sign_in user
-      visit edit_user_path
+      #sign_in user
+      visit edit_user_path(user)
     end
     describe "page" do
       it { should have_content("Update your profile") }
