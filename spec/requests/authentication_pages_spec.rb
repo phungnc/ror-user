caeea46e94868ae6d683934ed0285bf13a005a45
phungnc <phung.nc@aris-vn.com>
2014-05-13 19:09:56 +0700

    9.3.1 User index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,6 +33,7 @@ describe "AuthenticationPages" do
       before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -55,11 +56,38 @@ describe "AuthenticationPages" do
   end
 
   describe "authorization" do
+
+    describe "for not-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+
+    end
+
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
       describe "in the Users controller" do
         
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_title('Sign in') }
@@ -88,8 +116,6 @@ describe "AuthenticationPages" do
         before { patch user_path(wrong_user) }
         specify { expect(response).to redirect_to(root_url) }
       end
-
-
     end
 
        
