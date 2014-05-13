ff84582a8f3371a31b4941174a0b2af3fe9848fc
phungnc <phung.nc@aris-vn.com>
2014-05-13 20:42:56 +0700

    9.3.3 Pagination

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140511151808) do
     t.string   "remember_token"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name "Cao Phung"
-    email "caophung@gmail.com"
-    password "password"
-    password_confirmation "password"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
