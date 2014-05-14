9eac82e3e87b06d61d7ab300954795bf4029fc6a
phungnc <phungnc@gmail.com>
2014-05-14 07:54:10 +0700

    A test for forwarding to the default page after friendly forwarding.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -72,6 +72,19 @@ describe "AuthenticationPages" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+            it "should render default (Profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end
         end
       end
 
